f0ea98633880a771aed0367d1ab6733d2272852c
Ethan Harvey <ethan.harvey@tufts.edu>
2021-10-24 08:42:05 -0400

    Finished final TODOs

--- check_timestamp.py
+++ check_timestamp.py
@@ -4,18 +4,15 @@ import re
 import sys
 import getopt
 
-def check_timestamp(duedate, duetime, utln):
+def check_timestamp(duedate, duetime, path):
     
-    #ASSIGNMENT = os.environ.get("HW")
-    ASSIGNMENT = "hw2_MetroSim"
-    #if (utln = ""): UTLN = "/Users/ethanharvey/Desktop/check_timestamp/"
-    #else: UTLN = utln
-    UTLN = "/Users/ethanharvey/Desktop/check_timestamp/"
-    #GRADING_ROOT = os.environ.get("GRADING_ROOT")
-    GRADING_ROOT = "/Users/ethanharvey/Desktop/check_timestamp/grading"
+    ASSIGNMENT = os.environ.get("HW")
+    if (path = ""): CURRENT_DIRECTORY = os.getcwd()
+    else: CURRENT_DIRECTORY = path
+    GRADING_ROOT = os.environ.get("GRADING_ROOT")
 
     return_value = 0
-    files = os.listdir(UTLN)
+    files = os.listdir(CURRENT_DIRECTORY)
     
     # If no custom duedate and duetime were given
     if (duedate == "" and duetime == ""):
@@ -46,10 +43,16 @@ def check_timestamp(duedate, duetime, utln):
                 param_dictionary[param.split("=")[0]]=param.split("=")[1]
             assignment_dictionary_array.append(param_dictionary)
         # find the assignment duedate
+        assignment_duedate = ""
+        assignment_duetime = ""
         for assignment in assignment_dictionary_array:
             if assignment["assign"] == ASSIGNMENT:
                 assignment_duedate = assignment["duedate"]
                 assignment_duetime = assignment["duetime"]
+        if (assignment_duedate == ""):
+            print("Error: Assignment duedate not found")
+        if (assignment_duetime == ""):
+            print("Error: Assignment duetime not found")
     else:
         assignment_duedate = duedate
         assignment_duetime = duetime
@@ -63,6 +66,9 @@ def check_timestamp(duedate, duetime, utln):
             int(assignment_duetime.split(":")[0]),
             int(assignment_duetime.split(":")[1])
             )
+        # add 48 hours if duedate and duetime were not specified
+        if (duedate == "" and duetime == ""):
+            assignment_datetime = assignment_datetime + datetime.timedelta(days=2)
         for file in files:
             if (datetime.datetime.fromtimestamp(os.path.getmtime(UTLN+file))>assignment_datetime):
                 return_value = 1
@@ -75,6 +81,9 @@ def check_timestamp(duedate, duetime, utln):
             int(assignment_duetime.split(":")[0]),
             int(assignment_duetime.split(":")[1])
             )
+        # add 48 hours if duedate and duetime were not specified
+        if (duedate == "" and duetime == ""):
+            assignment_datetime = assignment_datetime + datetime.timedelta(days=2)
         for file in files:
             if (datetime.datetime.fromtimestamp(os.path.getmtime(UTLN+file))>assignment_datetime):
                 return_value = 1
@@ -89,29 +98,29 @@ def main():
     utln = ""
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hd:t:u",["help","duedate=","duetime=","utln="])
+        opts, args = getopt.getopt(sys.argv[1:],"hd:t:p",["help","duedate=","duetime=","path="])
     except getopt.GetoptError:
-        print('\ncheck_timestamp [-h] [-d DUEDATE & -t DUETIME] [-u UTLN]\n')
+        print('\ncheck_timestamp [-h] [-d DUEDATE & -t DUETIME] [-p PATH]\n')
         sys.exit(2)
         
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print("\nusage: check_timestamp [-h] [-d DUEDATE & -t DUETIME] [-u UTLN]\n")
+            print("\nusage: check_timestamp [-h] [-d DUEDATE & -t DUETIME] [-p PATH]\n")
             print("optional arguments:")
             print("-h, --help                Show this help message and exit")
             print("-d, --duedate DUEDATE     Date in form of MM/DD e.g. 12/25")
             print("-t, --duetime DUETIME     Time in form of HH:MM e.g. 15:00")
-            print("-u, --utln UTLN           Overrides student enviroment\n")
+            print("-p, --path PATH           Overrides student enviroment\n")
             sys.exit()
         elif opt in ("-d", "--duedate"):
             duedate = arg
         elif opt in ("-t", "--duetime"):
             duetime = arg
-        elif opt in ("-u", "--utln"):
+        elif opt in ("-p", "--path"):
             utln = arg
             
     if (duedate == "" and duetime) or (duedate and duetime == ""):
-        print('\ncheck_timestamp [-h] [-d DUEDATE & -t DUETIME] [-u UTLN]\n')
+        print('\ncheck_timestamp [-h] [-d DUEDATE & -t DUETIME] [-p PATH]\n')
         sys.exit(2)
         
     check_timestamp(duedate, duetime, utln)
